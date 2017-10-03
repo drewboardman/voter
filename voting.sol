6405d0dde74e8fef47d692899f69d722b0ad60cb
Mworker <cobra.kai@manheim.com>
2017-10-03 13:12:31 -0400

    starting templated voting class

---
+++ voting.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+contract Voting {
+  mapping (bytes32 => uint8) public votesReceived;
+  bytes32[] public candidateList;
+
+  function Voting(bytes32[] candidateNames) {
+    candidateList = candidateNames;
+  }
+}
